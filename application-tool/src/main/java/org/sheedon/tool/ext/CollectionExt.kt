a137f1c54267a006be2467ea55220ec87a838eca
Sheedon <sheedonsun@163.com>
2022-08-24 23:15:15 +0800

    新增Collection扩展类

---
+++ application-tool/src/main/java/org/sheedon/tool/ext/CollectionExt.kt
@@ -0,0 +1,28 @@
+package org.sheedon.tool.ext
+
+/**
+ * 集合扩展类
+ * @Author: sheedon
+ * @Email: sheedonsun@163.com
+ * @Date: 2022/8/24 22:28
+ */
+
+/**
+ * 将List<String>合并，并且以[regex]分割符
+ */
+fun Collection<String>.merge(regex: String): String {
+    if (this.isNullOrEmpty()) {
+        return ""
+    }
+
+    val builder = StringBuilder()
+    for (item in this) {
+        builder.append(item).append(regex)
+    }
+
+    if (builder.isNotEmpty()) {
+        builder.deleteCharAt(builder.length - 1)
+    }
+
+    return builder.toString()
+}
