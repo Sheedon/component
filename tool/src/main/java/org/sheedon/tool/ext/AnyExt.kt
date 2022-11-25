cb2241da94f3e03494f24234e34ce92a921c876a
Sheedon <sheedonsun@163.com>
2022-11-25 10:38:29 +0800

    更换空与非空操作实现

--- tool/src/main/java/org/sheedon/tool/ext/AnyExt.kt
+++ tool/src/main/java/org/sheedon/tool/ext/AnyExt.kt
@@ -15,10 +15,13 @@ fun Any.convertString(): String {
     return toString()
 }
 
-fun Any?.notNull(notNullAction: (value: Any) -> Unit, nullAction1: () -> Unit) {
+/**
+ * 判断是否为空 并传入相关操作
+ */
+inline fun <reified T> T?.notNull(notNullAction: (T) -> Unit, nullAction: () -> Unit = {}) {
     if (this != null) {
         notNullAction.invoke(this)
     } else {
-        nullAction1.invoke()
+        nullAction.invoke()
     }
 }
